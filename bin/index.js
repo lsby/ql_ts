0e181762355f9c868b19c272cf74f56f67b28ef8
hbybyyang <2451759073@qq.com>
2022-07-03 09:40:05 +0800

    debug

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 
 var exec = require("@lsby/exec_cmd").default;
+var path = require("path");
 
 async function main() {
   var 命令所在目录 = process.cwd();
+  var 临时目录 = path.resolve(命令所在目录, "tmp_ql_ts");
+  await exec(`mkdir ${临时目录}`);
   await exec(
-    `git clone --depth 1 https://github.com/lsby/Template_Typescript.git ${命令所在目录}`
+    `git clone --depth 1 https://github.com/lsby/Template_Typescript.git ${临时目录}`
   );
-  await exec(`rm -rf .git`);
+  await exec(`rm -rf ${临时目录}/.git`);
+  await exec(`mv ${临时目录}/{.[!.],}* ${命令所在目录}/`);
+  await exec(`rm -rf ${临时目录}`);
   console.log("安装npm依赖:", "npm i");
   console.log("监视编译:", "F1 -> 运行任务 -> watch:ts");
   console.log("运行服务:", "dev:service");
